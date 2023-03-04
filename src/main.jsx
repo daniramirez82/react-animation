a784edf812be4b07978c31db04f81f3f21e4f019
daniramirez82 <dani.ramirez.madrid@gmail.com>
2023-03-04 15:22:29 +0100

    hide bar

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,10 @@ import { App } from './app';
 import './main.scss';
 
 render(<App />, document.getElementById('app'));
+
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
